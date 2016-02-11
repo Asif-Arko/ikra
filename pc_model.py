a4519049bce789f0117e8629719a3bd0fb5f6443
Asif-Arko <arko_chowdhury33@yahoo.com>
2016-02-11 17:59:32 +0600

    Create pc_model.py

---
+++ pc_model.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+with open('/proc/cpuinfo') as f:
+    for line in f:
+        # Ignore the blank line separating the information between
+        # details about two processing units
+        if line.strip():
+            if line.rstrip('\n').startswith('model name'):
+                model_name = line.rstrip('\n').split(':')[1]
+                print(model_name)
