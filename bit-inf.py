52c8d182c7e309aa695d25570459cf34b1125ffe
Asif-Arko <arko_chowdhury33@yahoo.com>
2016-02-11 18:00:39 +0600

    Create bit-inf.py

---
+++ bit-inf.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+with open('/proc/cpuinfo') as f:
+    for line in f:
+        # Ignore the blank line separating the information between
+        # details about two processing units
+        if line.strip():
+            if line.rstrip('\n').startswith('flags') \
+                    or line.rstrip('\n').startswith('Features'):
+                if 'lm' in line.rstrip('\n').split():
+                    print('64-bit')
+                else:
+                    print('32-bit')
